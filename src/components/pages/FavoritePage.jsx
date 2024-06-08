14c1785144906ca3427d3b90f8a1061544fe7b6e
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-06-08 22:43:02 +0430

    complete UI design

--- src/components/pages/FavoritePage.jsx
+++ src/components/pages/FavoritePage.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import RecipeCard from "../RecipeCard";
 
 export default function FavoritePage() {
+  const fav = false;
   return (
-    <div>FavoritePage</div>
-  )
+    <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen">
+      <div className="max-w-screen-lg mx-auto">
+        <p className="font-bold text-3xl md:text-5xl mt-4">My Favorites</p>
+
+        {!fav && (
+          <div className="h-[80vh] flex flex-col items-center gap-4">
+            <img src="./404.svg" className="h-3/4 mt-14" alt="404 error" />
+          </div>
+        )}
+
+        {fav && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <RecipeCard/>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { Search } from "lucide-react";
+import React from "react";
+import RecipeCard from "../RecipeCard";
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div className="bg-[#faf9fb] p-10 flex-1">
+      <div className="max-w-screen-lg mx-auto">
+        <form>
+          <label className="input shadow-md flex items-center gap-2">
+            <Search size={"24"} />
+            <input
+              type="text"
+              className="text-sm md:text-md grow"
+              placeholder="What d you want cook today?"
+            />
+          </label>
+        </form>
+
+        <h1 className="font-bold text-3xl md:text-5xl mt-4">
+          Recommended Recipes
+        </h1>
+        <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">
+          Popular choices
+        </p>
+
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            <RecipeCard/>
+        </div>
+      </div>
+    </div>
+  );
 }
